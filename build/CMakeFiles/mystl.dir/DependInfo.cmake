d68b5d864b7bf97d5c3fb144fb91581d6cbdfc62
Ivan Jurin <nirujnavi@gmail.com>
2015-06-03 16:07:24 +0200

    dirty commit

---
+++ build/CMakeFiles/mystl.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/ivan/Documents/zavrsni/libs/mystl/src/daemon.cc" "/home/ivan/Documents/zavrsni/libs/mystl/build/CMakeFiles/mystl.dir/src/daemon.cc.o"
+  "/home/ivan/Documents/zavrsni/libs/mystl/src/io.cc" "/home/ivan/Documents/zavrsni/libs/mystl/build/CMakeFiles/mystl.dir/src/io.cc.o"
+  "/home/ivan/Documents/zavrsni/libs/mystl/src/signal_handlers.cc" "/home/ivan/Documents/zavrsni/libs/mystl/build/CMakeFiles/mystl.dir/src/signal_handlers.cc.o"
+  "/home/ivan/Documents/zavrsni/libs/mystl/src/string.cc" "/home/ivan/Documents/zavrsni/libs/mystl/build/CMakeFiles/mystl.dir/src/string.cc.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "../include"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
---
+++ build/CMakeFiles/mystl.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/mystl.dir/src/string.cc.o"
+  "CMakeFiles/mystl.dir/src/signal_handlers.cc.o"
+  "CMakeFiles/mystl.dir/src/io.cc.o"
+  "CMakeFiles/mystl.dir/src/daemon.cc.o"
+  "libmystl.pdb"
+  "libmystl.a"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/mystl.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
